6c8793b51a9f31e6fff38b5c6157a09de20e1d50
White Autumn <white@autumn.dev>
2025-03-06 10:16:30 +0100

    Change to use prefixKeys utility

--- src/typescript-tests.ts
+++ src/typescript-tests.ts
@@ -1,8 +1,10 @@
 import type { Linter } from 'eslint';
 
+import { prefixKeys } from './util/prefix-keys.js';
+
 export default () => ({
 	files: ['**/*.test.ts', '**/*.test.tsx', '**/*.test.mts', '**/*.test.cts'],
-	rules: {
-		'@typescript-eslint/no-unused-expressions': 'off'
-	}
+	rules: prefixKeys('@typescript-eslint', {
+		'no-unused-expressions': 'off'
+	})
 } satisfies Linter.FlatConfig);

5f827e2fe9d9299f5f4d55461dfc44e85ccaf5f9
White Autumn <white@autumn.dev>
2025-03-06 10:32:06 +0100

    Fix error for no-extraneous-dependencies in strict mode

--- src/import-order.ts
+++ src/import-order.ts
@@ -12,17 +12,19 @@ export default (options?: Options) => ({
 		import: importOrder
 	},
 
-	rules: prefixKeys('import', {
-		'no-mutable-exports': ['error'],
-		'order':              ['warn', {
-			groups: [
-				'builtin', 'external', 'internal', 'parent', 'sibling', 'index'
-			]
-		}],
+	rules: {
+		...prefixKeys('import', {
+			'no-mutable-exports': ['error'],
+			'order':              ['warn', {
+				groups: [
+					'builtin', 'external', 'internal', 'parent', 'sibling', 'index'
+				]
+			}]
+		}),
 
 		...(options?.strict !== true ? {} : prefixKeys('import', {
 			'no-extraneous-dependencies': ['warn']
 		}))
-	})
+	}
 
 } satisfies Linter.FlatConfig);

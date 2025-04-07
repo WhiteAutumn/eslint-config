a6c75f4b4c24106854bec94c4ad81908d2073713
White Autumn <white@autumn.dev>
2025-04-07 15:09:55 +0200

    Change disallowTypeAnnotations in @typescript-eslint/consistent-type-imports to false

--- src/typescript.ts
+++ src/typescript.ts
@@ -50,7 +50,7 @@ export default (options?: Options) => ({
 			'consistent-generic-constructors':         ['warn', 'constructor'],
 			'consistent-type-assertions':              ['warn', { assertionStyle: 'angle-bracket', objectLiteralTypeAssertions: 'allow' }],
 			'consistent-type-definitions':             ['warn', 'type'],
-			'consistent-type-imports':                 ['warn', { prefer: 'type-imports', fixStyle: 'separate-type-imports' }],
+			'consistent-type-imports':                 ['warn', { prefer: 'type-imports', fixStyle: 'separate-type-imports', disallowTypeAnnotations: false }],
 			'method-signature-style':                  ['warn', 'property'],
 			'no-array-constructor':                    ['warn'],
 			'no-array-delete':                         ['error'],
